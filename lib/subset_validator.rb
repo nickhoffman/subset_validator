7108498f52f0e9811ab24eda8e4dc04e1ec0b556
Nick Hoffman <nick@deadorange.com>
2010-11-22 21:54:41 -0500

    Added documentation to #validate_each .

--- lib/subset_validator.rb
+++ lib/subset_validator.rb
@@ -1,6 +1,34 @@
 class SubsetValidator < ActiveModel::EachValidator
   @@error_message = 'contains an invalid value'
 
+  # Validate that the specified attribute's value is a subset of another set.
+  #
+  # Note that this works on more than just arrays, too. It'll work on anything, provided that:
+  # * the attribute's value can be anything that responds to <i>#each</i> ;
+  # * the set of valid values can be anything that responds to <i>#include?</i> .
+  #
+  # @example
+  #   class User
+  #     include ActiveModel::Validations
+  #
+  #     attr_accessor :roles
+  #
+  #     @@valid_roles = %w(admin manager developer)
+  #
+  #     validates :roles, :subset => {:in => @@valid_roles}
+  #   end
+  #
+  #   u       = User.new
+  #   u.roles = %w(admin something_else)
+  #
+  #   u.valid?
+  #    => false
+  #   u.errors
+  #    => {:roles=>["contains an invalid value"]}
+  #
+  #   u.roles = %w(admin)
+  #   u.valid?
+  #    => true
   def validate_each(record, attribute, value)
     unless options[:in].respond_to? :include?
       raise ArgumentError, 'An object with the method include? must be supplied as the :in option of the configuration hash'
