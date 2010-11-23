5e91c2ffc0157526075da46c1a30931713a7d589
Nick Hoffman <nick@deadorange.com>
2010-11-22 19:31:28 -0500

    Raise an error if the :in option is not given.

--- lib/subset_validator.rb
+++ lib/subset_validator.rb
@@ -1,3 +1,9 @@
 class SubsetValidator < ActiveModel::EachValidator
   @@error_message = 'contains an invalid item'
+
+  def validate_each(record, attribute, value)
+    unless options[:in].respond_to? :include?
+      raise ArgumentError, 'An object with the method include? must be supplied as the :in option of the configuration hash'
+    end
+  end
 end
--- spec/subset_validator_spec.rb
+++ spec/subset_validator_spec.rb
@@ -1,5 +1,18 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+class UserTest
+  include ActiveModel::Validations
+
+  @@valid_permissions = %w(create retrieve update delete)
+
+  attr_accessor :missing_in
+  validates     :missing_in, :subset => true
+
+  def initialize(params = {})
+    params.each {|k, v| send "#{k}=", v}
+  end
+end
+
 describe SubsetValidator do
   it 'inherits from ActiveModel::EachValidator' do
     SubsetValidator.superclass.should equal ActiveModel::EachValidator
@@ -8,6 +21,17 @@ describe SubsetValidator do
   describe 'class variables' do
     it 'has @@error_message' do
       SubsetValidator.class_variables.should include :@@error_message
+    end
+  end
+
+  describe '#validate_each' do
+    describe "when the configuration hash's :in element is invalid" do
+      it 'raises an error' do
+        expect {UserTest.new(:missing_in => 'fails').valid?}.to raise_error(
+          ArgumentError,
+          'An object with the method include? must be supplied as the :in option of the configuration hash'
+        )
+      end
     end
   end
 end
